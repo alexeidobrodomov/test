9ffe6b83de43c35f4c22b4875c5a78b9e3ac9920
Alex D <alexeidobrodomov@gmail.com>
2025-01-12 03:16:10 +0300

    Update main files

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?php echo "hui_new"; ?>
-
-<?php echo "hui_2new"; ?>
-
-<?php echo "hui_3new"; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index File</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>This Index File</div>
+    </body>
+</html>
