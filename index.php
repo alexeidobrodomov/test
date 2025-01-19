9c91edc1b2496949abb878413b425ad4e4f00a16
Alex D <alexeidobrodomov@gmail.com>
2025-01-19 18:16:40 +0300

    Vers 4

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <div>This Index File. Hello World</div>
+        <div>This Index File. Hello Worlds</div>
     </body>
 </html>
